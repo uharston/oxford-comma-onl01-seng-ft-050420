573e64bde9e02ebf1529ae771d72e8ea5e572fbe
Uriah Harston <uharston@gmail.com>
2020-05-11 22:32:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 names = ["Uriah", "Laura", "Pascal", "Justine"]
 
+# There are different was to write this method. Notice 2 different ways.
+
+#First way that I wrote it
 def oxford_comma(array) #convert array into list of strings with the word "and" on the last array if more than 1
   if array.size > 2 #if array is more than 2
     x = 'and ' << array[-1] #modify the last element in the array by adding 'and' and saving that modified value to a variable
@@ -10,5 +13,20 @@ def oxford_comma(array) #convert array into list of strings with the word "and" 
     array.join(" and ") #convert array into strings adding " and " in between
   else # all other arrays go here(anything with 1 element)
     array.join #convert the 1 element array into a string
+  end
+end
+
+# Second way that I wrote it
+def oxford_comma(array)
+  if array.size >= 3
+    array[-1].insert(0, "and ") #insert 'and ' at the beginning of last element in the array
+    array.join(", ") #convert the modified array into a list of comma-separated strings
+  elsif array.size >= 2
+    # The 2 lines below also work, but array.join(" and ") does it simplier
+    # array[-1].insert(0, " and ")
+    # array.join
+    array.join(" and ") #convert the 2 element array adding an " and " in between
+  else
+    array.join
   end
 end
